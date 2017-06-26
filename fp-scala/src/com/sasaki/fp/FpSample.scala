dcbcf18fceb1f2c71dd3aeea3d7d666926a6858c
liuwei <wei.liu@suanhua.org>
2017-06-26 16:50:25 +0800

    implement isSorted.

--- fp-scala/src/com/sasaki/fp/FpSample.scala
+++ fp-scala/src/com/sasaki/fp/FpSample.scala
@@ -58,18 +58,10 @@ class Cafe {
   def coalesce(charges: List[Charge]): List[Charge] = charges.groupBy(_.card).values.map(_.reduce(_ combine _)).toList
 }
 
-// -------------------- P.11 Scala程序  ------------------------
+// --------------------  P.11 Scala程序   ------------------------
 object Module {
   // 求绝对值
   def abs(n: Int): Int = if (n < 0) -n else n
-
-  private def formatAbs(n: Int) = "The absolute value of %d is %d".format(n, abs(n))
-
-  def main(args: Array[String]): Unit = {
-    //    println(formatAbs(-5))
-    //    println(factorial(366))
-    //    println(tfib(6))
-  }
 
   // P.16 阶乘 
   def factorial(n: Int): Int = {
@@ -83,13 +75,64 @@ object Module {
   // 递归
   def fib(n: Int): Int = if (n <= 2) n - 1 else fib(n - 1) + fib(n - 2)
 
-  // 尾递归
+  // 尾递归 ???
   //  def tfib(n: Int): Int = {
   //    def local(n: Int, acc : Int, acc_ : Int): Int = if(n <= 2) n - 1 else local(n - 1, acc_, acc + acc_)
   //    local(n, 0, 1)
   //  }
+  
+  // ----------------------------  P.17 高阶函数  ------------------------------
+  private def formatAbs(x: Int) = "The absolute value of %d is %d".format(x, abs(x))
+  
+  private def formatFactorial(n: Int) = "The factorial of %d is %d".format(n, factorial(n))
+  
+  // 将formatAbs和formatFactorial泛化
+  def formapResult(name: String, n: Int, f: Int => Int/*接收函数参数*/) = "Thr %s of %d is %d".format(name, n, f(n))
+  
+  // 在数组中查找字符串的单态函数
+  def findFirst(array: Array[String], key: String) : Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int = if(n >= array.length) -1 else if(array(n) == key) n else loop(n + 1)
+    
+    loop(0)
+  }
+  
+  // 在数组中查找字符串的多态函数
+  def findFirst[T](array: Array[T], p: T => Boolean) : Int = {
+		  @annotation.tailrec
+		  def loop(n: Int): Int = if(n >= array.length) -1 else if(p(array(n))/*使用函数匹配当前元素*/) n else loop(n + 1)
+		  
+		  loop(0)
+  }
+  
+  // -------  P.20/练习2.2 实现isSorted方法，检测Array[A]是否按照给定的比较函数排序  --------
+  def isSorted[A](array: Array[A], ordered: (A, A) => Boolean) : Boolean = {
+    def loop(n: Int) : Boolean = {
+      if(array.length <= n + 1) 
+        true 
+      else 
+        if(ordered(array(n), array(n + 1))) loop(n + 1) else false
+    }
+    
+    loop(0)
+  }
+
+  def main(args: Array[String]): Unit = {
+    //    println(formatAbs(-5))
+    //    println(factorial(366))
+    //    println(tfib(6))
+    //     def ordered(x: Int, x_ : Int) = x < x_
+    //    println(isSorted(Array(9, 1, 3, 4), ordered))
+    
+    
+    println(isSorted(Array(1, 2, 3, 0), (x: Int, x_ : Int) => x < x_))
+  }
 
 }
+
+
+
+
 
 
 
