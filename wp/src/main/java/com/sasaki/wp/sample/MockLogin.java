bca9c403b1aae70626372e596298357a21dc8718
redskirt <redskirt@outlook.com>
2017-08-07 23:34:47 +0800

    未解决/digg 请求参数异常问题。

--- wp/src/main/java/com/sasaki/wp/sample/MockLogin.java
+++ wp/src/main/java/com/sasaki/wp/sample/MockLogin.java
@@ -7,31 +7,39 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.codec.CharEncoding;
+import org.apache.http.Consts;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
 import org.apache.http.cookie.CookieSpecProvider;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.cookie.BasicClientCookie;
 import org.apache.http.impl.cookie.BestMatchSpecFactory;
 import org.apache.http.impl.cookie.BrowserCompatSpecFactory;
+import org.apache.http.message.BasicNameValuePair;
 
 import sun.misc.BASE64Decoder;
 
@@ -55,9 +63,7 @@ public class MockLogin {
 	final static String FILE_PATH			= "C:\\Users\\wei.liu\\Desktop\\_\\t.png";
 	final static String CAPTCHA_REGEX		= "captcha: '(.+?)'";
 	final static String $toutiao_sso_user	= "toutiao_sso_user";
-//	static String cookieStr = "toutiao_sso_user=3460efc068605b814faa04dc84184be3; Domain=sso.toutiao.com; expires=Sun, 10-Sep-2017 06:15:16 GMT; httponly; Max-Age=3024000; Path=/"
-//			+ "sso_login_status=0; Domain=toutiao.com; expires=Sun, 10-Sep-2017 06:15:16 GMT; httponly; Max-Age=3024000; Path=/";
-	static String cookieStr = "csrftoken=099c9203c938060b4f1ea3dce16ab1a1; tt_webid=6447316118323512846; WEATHER_CITY=%E5%8C%97%E4%BA%AC; UM_distinctid=15d827c2ccf51-0963e2cc5326bc8-41554330-1fa400-15d827c2cd038c; CNZZDATA1259612802=407746919-1501128910-https%253A%252F%252Fwww.bing.com%252F%7C1502098437; uuid=\"w:82ca84223a28448cb7dfda2dfa5eab1c\"; sso_login_status=1; login_flag=0c02740c9e36917cafaabd4768f9ec29; sessionid=23db5f93ebc0295196623c8cbf22f3d1; uid_tt=18383eb38585d5a3988fa25d7eb5fe9a; sid_tt=23db5f93ebc0295196623c8cbf22f3d1; sid_guard=\"23db5f93ebc0295196623c8cbf22f3d1|1502084673|2591999|Wed\054 06-Sep-2017 05:44:32 GMT\"; __tasessionId=35k4l42191502098523992";
+	static String cookieStr = "UM_distinctid=15db62375bf520-07e30c1eda903a-143a6d54-13c680-15db62375c0424; uuid=\"w:aa24cc220e7a418bb6e4cffa8be3c448\"; login_flag=812f9a7d1d30496c2f308e906425d8cd; sessionid=3130dab04efadc6c8c35e871de3291bd; uid_tt=bd07b45d64ce252fe3fd023283ded123; sid_tt=3130dab04efadc6c8c35e871de3291bd; sid_guard=\"3130dab04efadc6c8c35e871de3291bd|1502011604|2591999|Tue\054 05-Sep-2017 09:26:43 GMT\"; sso_login_status=1; csrftoken=5c0e8529e5ad23b900963421010af5cb; WEATHER_CITY=%E5%8C%97%E4%BA%AC; tt_webid=6451038131775948302; CNZZDATA1259612802=1700012720-1501999169-%7C1502030762; __tasessionId=o5qe4kut81502032004628";
 	
 	public static void main(String[] args) {
 		CloseableHttpClient client = HttpClients.createDefault();
@@ -104,17 +110,17 @@ public class MockLogin {
 //			println(get0.getURI());
 //			client.execute(get0, context);
 		
-			HttpGet get1 = new HttpGet("https://www.toutiao.com/api/comment/list/?group_id=6450085980351578382&item_id=6450092422402671117&offset=5&count=10");
-			HttpResponse response1 = client.execute(get1, context);
-			int statusCode = response1.getStatusLine().getStatusCode();
-			if (statusCode == HttpStatus.SC_OK) {
-				HttpEntity entity1 = response1.getEntity();
-		        BufferedReader reader1 = new BufferedReader(new InputStreamReader(entity1.getContent(), CharEncoding.UTF_8));
-		        String line1 = null;
-		        while ((line1 = reader1.readLine()) != null) {
-		            println("https://www.toutiao.com/api/comment/list/?group_id=6450085980351578382&item_id=6450092422402671117&offset=5&count=10 --> " + line1);
-		        }
-			}
+//			HttpGet get1 = new HttpGet("https://www.toutiao.com/api/comment/list/?group_id=6450085980351578382&item_id=6450092422402671117&offset=5&count=10");
+//			HttpResponse response1 = client.execute(get1, context);
+//			int statusCode = response1.getStatusLine().getStatusCode();
+//			if (statusCode == HttpStatus.SC_OK) {
+//				HttpEntity entity1 = response1.getEntity();
+//		        BufferedReader reader1 = new BufferedReader(new InputStreamReader(entity1.getContent(), CharEncoding.UTF_8));
+//		        String line1 = null;
+//		        while ((line1 = reader1.readLine()) != null) {
+//		            println("https://www.toutiao.com/api/comment/list/?group_id=6450085980351578382&item_id=6450092422402671117&offset=5&count=10 --> " + line1);
+//		        }
+//			}
 				
 			
 //			URI uri2 = new URIBuilder()
@@ -157,22 +163,52 @@ public class MockLogin {
 //	        }
 			
 			
-			String postDigg = "comment_id=" + "1574781618691150" +
-							  "&dongtai_id=" + "1574781618691150" +
-							  "&group_id=" + "6450085980351578382" +
-							  "&item_id=" + "6450092422402671117" + 
-							  "&action=digg";
-			StringEntity postDiggEntity = new StringEntity(postDigg, CharEncoding.UTF_8);
+//			String postDigg = "comment_id=" + "1574781618691150" +
+//							  "&dongtai_id=" + "1574781618691150" +
+//							  "&group_id=" + "6450085980351578382" +
+//							  "&item_id=" + "6450092422402671117" + 
+//							  "&action=digg";
+			
+			List<NameValuePair> formData = new ArrayList<>();
+			formData.add(new BasicNameValuePair("comment_id", "1574799577432078"));
+			formData.add(new BasicNameValuePair("dongtai_id", "1574799577432078"));
+			formData.add(new BasicNameValuePair("group_id", "6450085980351578382"));
+			formData.add(new BasicNameValuePair("item_id", "6450092422402671117"));
+			formData.add(new BasicNameValuePair("action", "digg"));
+			HttpEntity nameValueEntity = new UrlEncodedFormEntity(formData, "UTF-8");
+			
+			String postDigg = "comment_id=1574799577432078&dongtai_id=1574799577432078&group_id=6450085980351578382&item_id=6450092422402671117&action=digg";
+			StringEntity postDiggEntity = new StringEntity(postDigg, ContentType.create("application/x-www-form-urlencoded", Consts.UTF_8));
 			HttpPost post = new HttpPost("https://www.toutiao.com/api/comment/digg/");
-			post.setEntity(postDiggEntity);
-			println("postDigg --> " + post.getURI());
+
+			post.setHeader("Connection", "keep-alive");
+			post.setHeader("Origin", "http://www.toutiao.com");
+			post.setHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36");
+			post.setHeader("Content-Type", "application/x-www-form-urlencoded");
+			post.setHeader("Accept", "text/javascript, text/html, application/xml, text/xml, */*");
+			post.setHeader("X-Requested-With:", "XMLHttpRequest");
+			post.setHeader("X-CSRFToken", "5c0e8529e5ad23b900963421010af5cb");
+			post.setHeader("Accept-Encoding", "gzip, deflate");
+			post.setHeader("Accept-Language", "ja,en-US;q=0.8,en;q=0.6");
+			
+			MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create();
+//			entityBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+			entityBuilder.addTextBody("comment_id", "1574799577432078");
+			entityBuilder.addTextBody("dongtai_id", "1574799577432078");
+			entityBuilder.addTextBody("group_id", "6450085980351578382");
+			entityBuilder.addTextBody("item_id", "6450092422402671117");
+			entityBuilder.addTextBody("action", "digg");
+			HttpEntity entity = entityBuilder.build();
+			
+			post.setEntity(nameValueEntity);
+			println("postDigg --> " + post.getURI() + " " + post.getEntity().getContentType());
 			HttpResponse response = client.execute(post, context);
 			HttpEntity hEntity = response.getEntity();
 	        println("----------------------------------------");
 	        println(response.getStatusLine());
 	        if(null != hEntity)
-	        	println("Response content length: " + hEntity.getContentLength());
-//	        // 打印结果
+	        		println("Response content length: " + hEntity.getContentLength());
+	        // 打印结果
 	        BufferedReader reader = new BufferedReader(new InputStreamReader(hEntity.getContent(), CharEncoding.UTF_8));
 	        String line_ = null;
 	        while ((line_ = reader.readLine()) != null) {
