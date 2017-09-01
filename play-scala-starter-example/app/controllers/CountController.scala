067c43af7309dacf59a59e90a746b7013a8ad700
git <redskirt@outlook.com>
2017-09-01 18:16:58 +0800

    _

--- play-scala-starter-example/app/controllers/CountController.scala
+++ play-scala-starter-example/app/controllers/CountController.scala
@@ -20,6 +20,7 @@ class CountController @Inject() (cc: ControllerComponents,
    * count. The result is plain text. This `Action` is mapped to
    * `GET /count` requests by an entry in the `routes` config file.
    */
+  
   def count = Action { Ok(counter.nextCount().toString) }
 
 }
