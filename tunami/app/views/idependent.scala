1586e1825b6d9e08500dffbc019f5f5a0a5e8ba0
Sasaki <redskirt@outlook.com>
2017-09-10 23:47:09 +0800

    login & index page view.

--- tunami/app/views/idependent.scala
+++ tunami/app/views/idependent.scala
@@ -4,8 +4,14 @@ package views
  * @Author Wei Liu
  * @Mail wei.liu@suanhua.org
  * @Timestamp 2017-09-08 下午2:34:34
- * @Description 
+ * @Description
  */
 class idependent {
- 
+  
+}
+
+object idependent {
+  val title = "TUNAMI后台管理系统 v1.0"
+  
+  def $(s: String) = controllers.routes.Assets.versioned(s)
 }
