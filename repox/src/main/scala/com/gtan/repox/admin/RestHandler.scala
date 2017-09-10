107a34f95ee500c79e0a9b4e13eccea4d81afc05
Sasaki <redskirt@outlook.com>
2017-09-11 06:52:40 +0800

    移动非自己开发的项目到三方库。

--- repox/src/main/scala/com/gtan/repox/admin/RestHandler.scala
+++
@@ -1,8 +0,0 @@
-package com.gtan.repox.admin
-
-import io.undertow.server.HttpServerExchange
-import io.undertow.util.HttpString
-
-trait RestHandler {
-  def route(implicit exchange: HttpServerExchange): PartialFunction[(HttpString, String), Unit]
-}
--- spark-redis-master/src/test/scala/com/redislabs/provider/redis/rdd/RedisTest.scala
+++
@@ -1,8 +0,0 @@
-package com.redislabs.provider.redis.rdd
-
-import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
-
-@RunWith(classOf[JUnitRunner])
-class RedisTest {
-}
