3b21b63ab917922cfe364934d10830fd29a747bb
git <redskirt@outlook.com>
2017-11-03 18:01:06 +0800

    _

---
+++ T/src/main/scala/org/sh/sbdp/slap/jp/annotation/TestAnnaotion.java
@@ -0,0 +1,26 @@
+package org.sh.sbdp.slap.jp.annotation;
+
+import java.lang.reflect.Field;
+
+/**
+ * @Author Wei Liu
+ * @Mail wei.liu@suanhua.org
+ * @Timestamp 2017年10月12日 下午3:08:46
+ * @Description 
+ * 
+ */
+public class TestAnnaotion {
+	@Attach2("name")
+	String name2;
+	@Attach2("id")
+	String id2;
+	
+	public static void main(String[] args) {
+		Field[] fields = TestAnnaotion.class.getDeclaredFields();
+		for (Field field : fields) {
+			Attach2 alias = field.getAnnotation(Attach2.class);
+			System.out.println(alias.value());
+		}
+		
+	}
+}
